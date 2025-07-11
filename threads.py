c98541312f679f988d292dcbf4c6d2d5d80f31cc
BomBatXD <yehonatancarmel105@gmail.com>
2025-07-12 02:29:46 +0300

    threads ex

---
+++ threads.py
@@ -0,0 +1,28 @@
+from threading import Thread
+counter = 0
+
+def increase_counter():
+    global counter
+    for _ in range(1000000):
+        counter += 1
+
+
+def decrease_counter():
+    global counter
+    for _ in range(1000000):
+        counter -= 1
+
+# Create two threads
+thread1 = Thread(target=increase_counter)
+thread2 = Thread(target=decrease_counter)
+
+# Start the threads
+thread1.start()
+thread2.start()
+
+# Wait for both threads to finish
+thread1.join()
+thread2.join()
+
+# Print the final value of the counter
+print("Final counter value: ", counter)
